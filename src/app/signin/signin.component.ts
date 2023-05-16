a80c351dbb0ff0bc529a97fb5f37a62598fdf589
vaaniicx <vanessakausl@gmail.com>
2023-05-16 15:33:38 +0200

    Implemented sign in business logic

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,12 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent implements OnInit {
+  title: String = 'Sign-In';
+
+  form_data: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  title: String = 'Sign-In';
-  email: String = '';
-  password: String = '';
-  password_confirm: String = '';
+  onSubmit(): void {
+    if (
+      this.form_data.controls['email'].value == 'test@test.at' &&
+      this.form_data.controls['password'].value == '123456789'
+    ) {
+      console.log('Login successful');
+    } else {
+      console.log('Login failed');
+    }
+  }
 }
