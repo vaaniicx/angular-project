bc6fb4c7618acdd43fabd9c3a7b501c1590c0fe0
vaaniicx <vanessakausl@gmail.com>
2023-05-17 10:01:26 +0200

    Removed import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule, provideRouter, withComponentInputBinding } from '@angular/router'; // CLI imports router
+import {
+  Routes,
+  RouterModule,
+  provideRouter,
+  withComponentInputBinding,
+} from '@angular/router'; // CLI imports router
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
   { path: 'sign-up', component: SignupComponent },
-  { path: "sign-in", component: SigninComponent }
+  { path: 'sign-in', component: SigninComponent },
 ]; // sets up routes constant where you define your routes
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forRoot(routes)
-  ],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [provideRouter(routes, withComponentInputBinding())]
+  providers: [provideRouter(routes, withComponentInputBinding())],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
